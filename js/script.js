a8b44f735b1c7e0b5f1b1383cf4e041d1997b8b8
Klaudia Chrzanowska <klaudia_chrzanowska@wp.pl>
2021-05-11 18:06:19 +0200

    Refactoring js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,66 @@
-console.log("Cześć Developerzy!");
-
-let amountPlnElement = document.querySelector(".js-amountPln");
-let rateEurElement = document.querySelector(".js-rateEur");
-let rateUsdElement = document.querySelector(".js-rateUsd");
-let rateGbpElement = document.querySelector(".js-rateGbp");
-let rateChfElement = document.querySelector(".js-rateChf");
-let formElement = document.querySelector(".js-form");
-let exchangeElement = document.querySelector(".js-exchange");
-let selectedCurrencyElement = document.querySelector(".js-selectedCurrency")
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amountPln = amountPlnElement.value;
-    let rateEur = rateEurElement.value;
-    let rateUsd = rateUsdElement.value;
-    let rateGbp = rateGbpElement.value;
-    let rateChf = rateChfElement.value;
-    let selectedCurrency = selectedCurrencyElement.value;
-    let exchange = 0;
-
-    switch (selectedCurrency) {
-        case "EUR":
-            exchange = amountPln / rateEur;
-            break;
-        case "USD":
-            exchange = amountPln / rateUsd;
-            break;
-        case "GBP":
-            exchange = amountPln / rateGbp;
-            break;
-        case "CHF":
-            exchange = amountPln / rateChf;
-            break;
-        default:
-            console.log("Coś poszło nie tak!")
-            break;
+{
+    let currencyName = "";
+    const welcome = () => {
+        console.log("Cześć Developerzy!");
     }
-    exchangeElement.innerText = exchange.toFixed(2) + 'zł';
-});
+
+    welcome()
+
+    const calculateResult = (amountPln, selectedCurrency) => {
+
+
+        const rateEurElement = document.querySelector(".js-rateEur");
+        const rateUsdElement = document.querySelector(".js-rateUsd");
+        const rateGbpElement = document.querySelector(".js-rateGbp");
+        const rateChfElement = document.querySelector(".js-rateChf");
+        const rateEur = rateEurElement.value;
+        const rateUsd = rateUsdElement.value;
+        const rateGbp = rateGbpElement.value;
+        const rateChf = rateChfElement.value;
+
+        switch (selectedCurrency) {
+            case "EUR":
+                currencyName = "EUR";
+                return amountPln / rateEur;
+
+            case "USD":
+                currencyName = "USD";
+                return amountPln / rateUsd;
+
+            case "GBP":
+                currencyName = "GBP";
+                return amountPln / rateGbp;
+
+            case "CHF":
+                currencyName = "CHF";
+                return amountPln / rateChf;
+
+            default:
+                console.log("Coś poszło nie tak!")
+                break;
+        }
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const exchangeElement = document.querySelector(".js-exchange");
+        const selectedCurrencyElement = document.querySelector(".js-selectedCurrency");
+        const amountPlnElement = document.querySelector(".js-amountPln");
+        const selectedCurrency = selectedCurrencyElement.value;
+        const amountPln = amountPlnElement.value;
+
+        const result = calculateResult(amountPln, selectedCurrency);
+
+        exchangeElement.innerText = result.toFixed(2) + ' ' + currencyName;
+    };
+
+    const init = () => {
+
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+    init();
+}
 
